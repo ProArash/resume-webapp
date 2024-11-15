c1e2b6fbbe50ec05e14fced9a927a82809e7b902
Arash <arashdev78@gmail.com>
2024-11-15 22:50:20 +0330

    fixed mobile responsive

--- src/components/ProfileBox/ProfileBox.tsx
+++ src/components/ProfileBox/ProfileBox.tsx
@@ -16,28 +16,30 @@ const ProfileBox = ({
 	email: string;
 }) => {
 	return (
-		<MainContainer className="items-center">
-			<div className={`flex rounded-full ring ring-onPrimary`}>
-				<Image
-					className="object-contain w-[150px] h-[150px] rounded-full"
-					src={avatar}
-					alt="Me :D"
-				/>
-			</div>
-			<div className="flex flex-col gap-2">
-				<h1>{name}</h1>
-				<p>
-					<FaCode />
-					{stackName}
-				</p>
-				<p>
-					<FaMobile />
-					{mobile}
-				</p>
-				<p>
-					<FaInbox />
-					{email}
-				</p>
+		<MainContainer>
+			<div className="flex md:flex-row flex-col gap-2 items-center">
+				<div className={`flex rounded-full ring ring-onPrimary`}>
+					<Image
+						className="object-contain w-[150px] h-[150px] rounded-full"
+						src={avatar}
+						alt="Me :D"
+					/>
+				</div>
+				<div className="flex flex-col gap-2">
+					<h1>{name}</h1>
+					<p>
+						<FaCode />
+						{stackName}
+					</p>
+					<p>
+						<FaMobile />
+						{mobile}
+					</p>
+					<p>
+						<FaInbox />
+						{email}
+					</p>
+				</div>
 			</div>
 		</MainContainer>
 	);
